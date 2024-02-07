b892a2cd4fc47a794e5ade85ddeebd60d5e6e045
mohammedbouakka <158282999+mohammedbouakka@users.noreply.github.com>
2024-02-07 10:23:25 +0100

    Create bateria3.py

---
+++ bateria3.py
@@ -0,0 +1,8 @@
+# Obtenir un número de l'usuari
+numero = int(input("Introdueix un número: "))
+
+# Comprovar si el número és senar o parell
+if numero % 2 == 0:
+    print(f"El número {numero} és parell.")
+else:
+    print(f"El número {numero} és senar.")
