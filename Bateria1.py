720ab876e6b10249bbcf27d25a759edfc81144b6
mohammedbouakka <158282999+mohammedbouakka@users.noreply.github.com>
2024-02-07 10:20:35 +0100

    Update Bateria1.py

--- Bateria1.py
+++ Bateria1.py
@@ -1,25 +1,9 @@
-<!DOCTYPE html>
-<html lang="ca">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Benvinguda</title>
-  <script>
-    function comprovarUsuari() {
-      var nomUsuari = document.getElementById("nom").value;
+# Obtenir el nom de l'usuari
+nom_usuari = input("Introdueix el teu nom: ")
 
-      if (nomUsuari === "Amilcar" || nomUsuari === "Sila") {
-        alert("Benvingut, " + nomUsuari + "!");
-      } else {
-        alert("Nom d'usuari no vàlid. Només es permeten 'Amilcar' o 'Sila'.");
-      }
-    }
-  </script>
-</head>
-<body>
-  <h1>Benvinguda</h1>
-  <label for="nom">Introdueix el teu nom:</label>
-  <input type="text" id="nom" name="nom">
-  <button onclick="comprovarUsuari()">Verificar</button>
-</body>
-</html>
+# Comprovar si l'usuari és "Amilcar" o "Sila"
+if nom_usuari == "Amilcar" or nom_usuari == "Sila":
+    print(f"Benvingut, {nom_usuari}!")
+else:
+    print("Nom d'usuari no vàlid. Només es permeten 'Amilcar' o 'Sila'.")
+
